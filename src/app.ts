a26a7fac2f038916e240431316dcbc27326741a1
Tsolovikos Theologos <teotsolos@hotmail.gr>
2023-08-26 13:04:47 +0300

    chore(dev) set logging tool, chore(app.ts) set up api rules and handle error chore(controller) create controllers setup

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,51 @@
 //Imports
-import express, { Express } from 'express';
+import express, { Express, Request, Response, NextFunction} from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 import path from 'path';
 import routes from './routes';
 
 //Initialize configuration 
 dotenv.config();
-
+//Init App
 const app: Express = express();
+//Local Server port 
+const port: any = process.env.PORT || 3000;
 //Setting the root path for views directory
 app.set('views', path.join(__dirname, 'views'));
 //Setting the view engine
 app.set('view engine', 'ejs');
 //Set Static content serve
 app.use(express.static(path.join(__dirname, "public")));
+/** Parse the request */
+app.use(express.urlencoded({ extended: false }));
+/** Takes care of JSON data */
 app.use(express.json());
+/** Logging */
+app.use(morgan('dev'));
 
-//Local Server port 
-const port: any = process.env.PORT || 3000;
+/** RULES OF OUR API */
+app.use((req : Request, res : Response, next: NextFunction)  => {
+    // set the CORS policy
+    res.header('Access-Control-Allow-Origin', '*');
+    // set the CORS headers
+    res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With, Content-Type, Accept, Authorization');
+    // set the CORS method headers
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET POST');
+        return res.status(200).json({});
+    }
+    next();
+});
 //Set Endpoints
 app.use('/', routes);
-
+/** Error handling */
+app.use((req : Request, res : Response, next: NextFunction) => {
+    const error = new Error('Not found');
+    return res.status(404).json({
+        message: error.message
+    });
+});
 //Server Start Point
 app.listen(port, process.env.IP as string, () => {
     console.log(`Server has started on ${port} PORT ...press ctrl+C to stop!`);
---
+++ src/controllers/api.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import axios, { AxiosResponse } from 'axios';
+
+//Set Types <interface></interface>
+
+
+
+// method get all breads to json
+const getBreedList = null;
+
+// method get bread:name info
+const getBreedInfo = null;
+
+// method get dog fact
+const getDogFact = null;
+
+//Exports
+export default { getBreedList, getBreedInfo, getDogFact};
